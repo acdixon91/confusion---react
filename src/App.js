f27a09b9c362f0db5dd370fdf9737efe47da86a3
Andrew <acdixon91@gmail.com>
2020-11-21 19:17:38 -0700

    lifecycle pt1

--- src/App.js
+++ src/App.js
@@ -1,50 +1,35 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/menuComponent";
-import './App.css';
+import "./App.css";
 import { DISHES } from "./shared/dishes";
 
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
     };
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <React.StrictMode>
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
-      </div>
+        <div>
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu dishes={this.state.dishes} />
+        </div>
       </React.StrictMode>
     );
   }
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /* Old code */
 
